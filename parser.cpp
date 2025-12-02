5c9fb47cd1bc7fec49191fb29aa4635dc964f4ae
Sarthak Khandelwal <mastermind.sarthak@gmail.com>
2025-12-02 18:33:47 -0500

    add: stage 1 v1

--- parser.cpp
+++ parser.cpp
@@ -1,5 +1,8 @@
 #include <immintrin.h>
 #include <bitset>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include "utils.h"
 #include "parser.h"
@@ -55,6 +58,50 @@ namespace ParaJson {
     }
 
 
+    void extract_structural_whitespace_characters_scalar(
+        const char* raw,         // input 64 bytes
+        uint64_t literal_mask,   // bits of chars inside quotes
+        uint64_t *structural_mask,
+        uint64_t *whitespace_mask)
+    {
+        uint64_t struct_mask = 0;
+        uint64_t ws_mask = 0;
+
+        for (int i = 0; i < 64; i++) {
+            char c = raw[i];
+
+            // check structural
+            bool is_structural = (strchr("{}[]:,", c) != NULL);
+            if (is_structural) {
+                struct_mask |= (1ULL << i);
+            }
+
+            // check whitespace
+            bool is_ws = (strchr(" \t\r\n", c) != NULL);
+            if (is_ws) {
+                ws_mask |= (1ULL << i);
+            }
+        }
+
+        // remove characters inside string literals
+        *structural_mask = struct_mask & ~literal_mask;
+        *whitespace_mask = ws_mask & ~literal_mask;
+    }
+
+    uint64_t extract_pseudo_structural_mask(
+            uint64_t structural_mask, uint64_t whitespace_mask, uint64_t quote_mask, uint64_t literal_mask,
+            uint64_t *prev_pseudo_structural_end_mask) {
+        uint64_t st_ws = structural_mask | whitespace_mask;
+        structural_mask |= quote_mask;
+        uint64_t pseudo_structural_mask = (st_ws << 1U) | *prev_pseudo_structural_end_mask;
+        *prev_pseudo_structural_end_mask = st_ws >> 63U;
+        pseudo_structural_mask &= (~whitespace_mask) & (~literal_mask);
+        structural_mask |= pseudo_structural_mask;
+        structural_mask &= ~(quote_mask & ~literal_mask);
+        return structural_mask;
+    }
+
+
     JSON::JSON(char *document, size_t size,  bool manual_construct) {
         input = document;
         input_len = size;
@@ -80,20 +127,24 @@ namespace ParaJson {
             char* warp = (input + offset);
             uint64_t escape_mask = extract_escape_mask(warp, &prev_escape_mask);
             uint64_t literal_mask = extract_literal_mask(warp, escape_mask, &prev_quote_mask, &quote_mask);
-            std::cout << (std::bitset<64>(literal_mask)) << "\n";
+            
             // Dump pointers for *previous* iteration.
             construct_structural_character_pointers(pseudo_mask, offset - 64, indices, &num_indices);
 
-            // extract_structural_whitespace_characters(warp, literal_mask, &structural_mask, &whitespace_mask);
-            // pseudo_mask = extract_pseudo_structural_mask(
-            //         structural_mask, whitespace_mask, quote_mask, literal_mask, &prev_pseudo_mask);
+            extract_structural_whitespace_characters_scalar(warp, literal_mask, &structural_mask, &whitespace_mask);
+            pseudo_mask = extract_pseudo_structural_mask(
+                    structural_mask, whitespace_mask, quote_mask, literal_mask, &prev_pseudo_mask);
         }
         // Dump pointers for the final iteration.
-        // construct_structural_character_pointers(pseudo_mask, offset - 64, indices, &num_indices);
-        // if (num_indices == 0 || input[indices[num_indices - 1]] != '\0')  // Ensure '\0' is added to indices.
-        //     indices[num_indices++] = offset - 64 + strlen(input + offset - 64);
-        // if (prev_quote_mask != 0)
-        //     throw std::runtime_error("unclosed quotation marks");
+        construct_structural_character_pointers(pseudo_mask, offset - 64, indices, &num_indices);
+        if (num_indices == 0 || input[indices[num_indices - 1]] != '\0')  // Ensure '\0' is added to indices.
+            indices[num_indices++] = offset - 64 + strlen(input + offset - 64);
+        if (prev_quote_mask != 0)
+            throw std::runtime_error("unclosed quotation marks");
+        std::cout << "Number of indices: " << num_indices << "\n";
+        for (size_t i = 0; i < num_indices; ++i) {
+            std::cout << "Index " << i << ": " << indices[i] << " ('" << input[indices[i]] << "')\n";
+        }
     }
 
     // void JSON::exec_stage_1() {
